8a8331e525a05069471de2332bb451e7c45dfcff
Akshay waghmode <awaghmode@s2datasystems.in>
2025-12-05 01:02:26 +0530

    updated pipeline

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pickle
+from mangum import Mangum
 model=pickle.load(open("model.pkl","rb"))
 app=Flask(__name__)
 handler=Mangum(app)
